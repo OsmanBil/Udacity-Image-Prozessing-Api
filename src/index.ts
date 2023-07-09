690781b34647ecf750a98009b7a4e9da1cdfca42
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-07-09 20:49:32 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+import sharp from 'sharp';
 
 const app = express();
 const port = 3000;
@@ -6,6 +8,26 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('server is working');
 });
+
+
+
+app.get('/don', (req, res) => {
+  const { filename, width, height } = req.query;
+  const inputPath = path.join(__dirname, 'images/input.jpg');
+  const outputPath = `${__dirname}/images/output.jpg`;
+
+  sharp(inputPath)
+  .resize(Number(width), Number(height))
+  .toFile('src/images/output.jpg', (err, info) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(info);
+      res.sendFile(outputPath);
+    }
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`);
